f4ebc4ae9d087cea0bd501103e22f455971071b1
Kirill Berezin <berezinkb06@gmail.com>
2024-05-01 01:16:25 +0800

    basic matrix generation

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
 import GameField from "@/components/GameField.vue";
-import { matchedRouteKey } from "vue-router";
-import { ref } from "vue";
-const matrix = ref(Array(9).fill(Array(9).fill(0)));
 
-function createMatrix() {
+function createBasicMatrix() {
+  let matrix = [];
   for (let i = 0; i < 9; i++) {
+    matrix[i] = Array(0);
     for (let j = 0; j < 9; j++) {
-      matrix.value[i][j] = Math.round(Math.random() * 9);
-      console.log(i, j, matrix.value[i][j]);
+      matrix[i][j] = Math.floor(((i * 3 + i / 3 + j) % 9) + 1);
     }
   }
-  console.log(matrix.value);
+  return matrix;
 }
 
-createMatrix();
+let matrix = createBasicMatrix();
 </script>
 
 <template>
